de9b1195f2adba0f25ff024de7635a8bc8143385
HEXEDbyHEIS <maropraise071@gmail.com>
2026-03-07 12:53:37 +0100

    Add boredom button with random activity alert

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to Safe Haven 💙 A safe space just for teens!");
 });
+const boredBtn = document.getElementById("boredBtn");
+
+boredBtn.addEventListener("click", function () {
+
+const activities = [
+"Watch a funny video",
+"Take a quick quiz",
+"Play a mini game",
+"Read a short story",
+"Listen to a new song"
+];
+
+const random = Math.floor(Math.random() * activities.length);
+
+alert("Try this: " + activities[random]);
+
+});
